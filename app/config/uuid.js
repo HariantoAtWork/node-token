b87079c927225d27a05805bdc69aa889fcc3ccd9
Harianto van Insulinde <hariantoatwork@gmail.com>
2016-08-05 23:28:30 +0200

    Simplefied for getting uuid

--- app/config/uuid.js
+++ app/config/uuid.js
@@ -1,11 +1,2 @@
-var _uuid   = require('node-uuid');
-var Memory = require('../core/memory');
-
-var uuid = Memory.get('uuid');
-console.log('-- Memory - uuid: '+uuid);
-if (!uuid) {
-	uuid = Memory.set('uuid', _uuid.v4());
-}
-
-
-module.exports = uuid.v4();
+// App UUID random generated on each restart
+module.exports = require('node-uuid').v4();
